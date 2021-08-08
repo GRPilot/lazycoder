f36efefd339cd67d4ffe2d214086228446be47a1
Ruslan Golovinsky <rusan.rusik@yandex.ru>
2021-08-08 19:07:03 +0300

    initial commit

---
+++ lang/lazycoder_en_US.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/mainwindow/mainwindow.ui" line="14"/>
+        <location filename="../build/build-lazycoder-Desktop_Qt_6_1_2_MinGW_64_bit-Debug/lazycoder_autogen/include/ui_mainwindow.h" line="50"/>
+        <location filename="../build/Debug-lazycoder-Desktop_Qt_6_1_2_MinGW_64_bit/lazycoder_autogen/include/ui_mainwindow.h" line="50"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
