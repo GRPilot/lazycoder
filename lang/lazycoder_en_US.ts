8ddd9e7bf9683a2874961aa30331763c11111505
Ruslan Golovinsky <rusan.rusik@yandex.ru>
2021-08-09 12:28:41 +0300

    update version

--- lang/lazycoder_en_US.ts
+++ lang/lazycoder_en_US.ts
@@ -6,6 +6,7 @@
     <message>
         <location filename="../src/mainwindow/mainwindow.ui" line="14"/>
         <location filename="../build/Windows/Debug/lazycoder_autogen/include/ui_mainwindow.h" line="50"/>
+        <location filename="../build/Windows/Release/lazycoder_autogen/include/ui_mainwindow.h" line="50"/>
         <source>MainWindow</source>
         <translation type="unfinished"></translation>
     </message>
