3988b8d4ba8395e91b77210f3ccbc11dc822baaa
Ruslan Golovinsky <rusan.rusik@yandex.ru>
2021-08-15 12:51:44 +0300

    add document's author, locale and translates

--- lang/lazycoder_ru_RU.ts
+++ lang/lazycoder_ru_RU.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en_US">
+<TS version="2.1" language="ru_RU">
 <context>
     <name>MainWindow</name>
     <message>
@@ -9,15 +9,15 @@
     </message>
     <message>
         <source>Menu</source>
-        <translation>Menu</translation>
+        <translation>Меню</translation>
     </message>
     <message>
         <source>Settings</source>
-        <translation>Settings</translation>
+        <translation>Настройки</translation>
     </message>
     <message>
         <source>Version %1 | Copyright seigtm x gollan | From 2021 with love 🥰</source>
-        <translation>Version %1 | Copyright: seigtm x gollan | From 2021 with love 🥰</translation>
+        <translation>Версия %1 | Правообладатели: seigtm x gollan | Из 2021 с любовью 🥰</translation>
     </message>
 </context>
 </TS>
