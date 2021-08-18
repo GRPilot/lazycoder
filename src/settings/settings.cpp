b1d6d78aa3d00eb61bfab78024e331c92ebd1027
Костыль <seigtm@gmail.com>
2021-08-18 15:42:20 +0300

    settings.cpp created and minor qCSS fixes in mainWindow.ui

---
+++ src/settings/settings.cpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <QSettings>
+#include <QStandardPaths>
+
+namespace setm {
+QSettings &Settings() {
+    static QSettings settings(
+        QStandardPaths::writableLocation(QStandardPaths::AppConfigLocation) + "/settings.ini",
+        QSettings::IniFormat);
+
+    return settings;
+}
+}  // namespace setm
