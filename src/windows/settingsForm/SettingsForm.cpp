bc0a5bb634db1234b2c2fde03ea22b37a0415625
Костыль <seigtm@gmail.com>
2021-08-30 16:54:20 +0300

    Removed debug trash

--- src/windows/settingsForm/SettingsForm.cpp
+++ src/windows/settingsForm/SettingsForm.cpp
@@ -9,27 +9,12 @@ SettingsForm::SettingsForm(QWidget *parent)
 {
     ui->setupUi(this);
 
+/*
     auto app{ Utils::getSettings("app") };
     if(nullptr == app) {
         throw int(-1);
     }
-    app->setValue("theme", "white");
-    app->setValue("lang", "english");
-    app->setValue("default_dir", "some_directory");
-    app->setValue("repos", QStringList({"repo1", "repo2", "repo3"}));
-    app->setValue("autosync", true);
-
-    auto theme{ app->value("theme") };
-    auto lang{ app->value("lang") };
-    auto defaultDir{ app->value("default_dir") };
-    auto reposList{ app->value("repos") };
-    auto isAutoSync{ app->value("autosync") };
-
-    qDebug() << theme.typeName() << " "
-             << lang.typeName() << " "
-             << defaultDir.typeName() << " "
-             << reposList.typeName() << " "
-             << isAutoSync.typeName();
+*/
 }
 
 SettingsForm::~SettingsForm()
